8b542073caae0634a78e6ea9e789576ac568c410
Ekjot Kaur <ekjotkaurk13@gmail.com>
2021-07-07 14:27:49 +0530

    logout feature added

--- client/src/components/Navbars/HeadBar/HeadBar.js
+++ client/src/components/Navbars/HeadBar/HeadBar.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Dropdown } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
+import { UserContext } from "../../../App";
 
 import "./HeadBar.css";
 
 const HeadBar = (props) => {
+  const { state, dispatch } = useContext(UserContext);
+  const history = useHistory();
+
   return (
     <div className="headBar">
       <div className="headBarBox">
@@ -15,8 +21,54 @@ const HeadBar = (props) => {
           <div className="headBarMSTeams">Microsoft Teams</div>
         </div>
         <div className="headBarSearch">
-          <input type="text" className="headerBarSearchInput" placeholder="Search" />
+          <input
+            type="text"
+            className="headerBarSearchInput"
+            placeholder="Search"
+          />
         </div>
+        {state && (
+          <div className="headerBarProfile">
+            {/* <div className="headerBarProfileImg">
+            {state.name.match(/\b(\w)/g).join("")}
+          </div> */}
+            <Dropdown>
+              <Dropdown.Toggle
+                className="headerBarProfileImg"
+                variant="success"
+                id="dropdown-basic"
+              >
+                {state.name.match(/\b(\w)/g).join("")}
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu className="dropdownBody">
+                <div className="profileOfUser">
+                  <div className="profileOfUserImg">
+                    {state.name.match(/\b(\w)/g).join("")}
+                  </div>
+                  <div className="profileOfUserImgDetails">
+                    <div className="profileOfUserImgDetailsName">
+                      {state.name}
+                    </div>
+                    <div className="profileOfUserImgDetailsEmail">
+                      {state.email}
+                    </div>
+                  </div>
+                </div>
+                <div
+                  className="signout"
+                  onClick={() => {
+                    localStorage.clear();
+                    dispatch({ type: "CLEAR" });
+                    history.push("/");
+                  }}
+                >
+                  Sign Out
+                </div>
+              </Dropdown.Menu>
+            </Dropdown>
+          </div>
+        )}
       </div>
     </div>
   );
